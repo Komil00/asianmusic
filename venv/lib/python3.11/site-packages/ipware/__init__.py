0704f685ac22c8865c4038d12a1b4e0aae32a852
Komil Tuev <mirjon@MacBook-Pro-Komil.local>
2023-02-11 23:47:58 +0500

    update gitignore

--- venv/lib/python3.11/site-packages/ipware/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ip import get_client_ip  # noqa
-
-default_app_config = 'ipware.apps.IPwareConfig'
--- venv/lib/python3.11/site-packages/marshmallow_jsonapi/__init__.py
+++
@@ -1,4 +0,0 @@
-from .schema import Schema, SchemaOpts
-
-__version__ = "0.24.0"
-__all__ = ("Schema", "SchemaOpts")
